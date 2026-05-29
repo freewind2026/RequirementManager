3cec9dd2702063cdca66d14338f1f2eea56e128b
david2025 <lndldavid@sina.com>
2026-05-29 21:50:43 +0800

    fix: add frontend_dist to STATICFILES_DIRS for Django dev static serving

--- backend/requirement_manager/settings.py
+++ backend/requirement_manager/settings.py
@@ -85,6 +85,9 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATICFILES_DIRS = [
+    BASE_DIR.parent / 'frontend_dist',
+]
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 
